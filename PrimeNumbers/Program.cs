44234bc30ad04888edc89adf9038568f4274acbd
Aaron Merrill <aaronjmerrill@hotmail.com>
2018-02-10 17:28:55 -0600

    Skeleton code with validation. SImilar to FizzBuzz, but a little different.

--- PrimeNumbers/Program.cs
+++ PrimeNumbers/Program.cs
@@ -6,7 +6,27 @@ namespace PrimeNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int limit = GetLimit();
+            if (limit == int.MinValue)
+                Console.Write("Invalid limit");
+            else
+            {
+                Console.WriteLine("2");
+                for (int i = 3; i <= limit; i+=2)
+                {
+                   
+                }
+            }
+            Console.ReadLine();
+        }
+        private static int GetLimit()
+        {
+            Console.Write("Limit: ");
+            string sLimit = Console.ReadLine();
+            int nLimit;
+            if (!int.TryParse(sLimit, out nLimit) || nLimit < 2)
+                nLimit = int.MinValue;
+            return nLimit;
         }
     }
 }
